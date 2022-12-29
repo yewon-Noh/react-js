cbaa7c517ec67f85a4868eec3f36a3e0fe91e0a5
yewon-Noh <yewo2nn16@gmail.com>
2022-12-29 17:21:22 +0900

    feat : jsx

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Library from './jsx/Library';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
 );
 
