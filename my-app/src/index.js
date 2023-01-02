457528777fdfe3d650aee14fc4b32db554643e77
yewon-Noh <yewo2nn16@gmail.com>
2023-01-02 23:09:53 +0900

    feat : state and lifecycle

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import CommentList from './props/CommentList';
+import NotificationList from './state/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>
 );
 
